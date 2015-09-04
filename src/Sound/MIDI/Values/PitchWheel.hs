bcdddd81f1d6c6f878ef9a3ee0c96101f0c6e264
Jon Childress <jon@childr.es>
2015-09-04 12:12:39 -0500

    midi values cleanup

--- src/Sound/MIDI/Values/PitchWheel.hs
+++ src/Sound/MIDI/Values/PitchWheel.hs
@@ -16,15 +16,16 @@ import Sound.Midi.Internal.Types (PitchWheel (..))
 
 highest   = pitchWheel 100
 higher    = pitchWheel 66.6
-high      = pitchWheel 33.
+high      = pitchWheel 33.3
 centered  = pitchWheel 0
 low       = pitchWheel (-33.3)
 lower     = pitchWheel (-66.6)
 lowest    = pitchWheel (-100)
 
 pitchWheel :: Float -> PitchWheel
-pitchWheel 0 = centered
-pitchWheel n = PitchWheel . splitBytes $ correlateRanges (-100,100) (0,16383) n
+pitchWheel = PitchWheel . splitBytes . correlateRanges (-100,100) (0,16383)
+
+-- private functions
 
 -- the PitchWheel value is is a 14-bit uint represented by two 8-bit uints,
 -- where the first byte's bits 0-6 are the value's bits 0-6 and the second
