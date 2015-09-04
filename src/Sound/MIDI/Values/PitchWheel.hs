620c905cbf32dad070ad94c80bbbac06471a1cf0
Jon Childress <jon@childr.es>
2015-09-04 12:07:59 -0500

    use pitchWheel function for clearer value separation

--- src/Sound/MIDI/Values/PitchWheel.hs
+++ src/Sound/MIDI/Values/PitchWheel.hs
@@ -9,27 +9,25 @@ module Sound.Midi.Values.PitchWheel
 , pitchWheel
 ) where
 
-import Data.Bits ((.|.), shiftR)
+import Data.Bits ((.|.), shiftL, shiftR)
 import Data.Word (Word16)
 import Sound.Midi.Internal.Ops (correlateRanges, mask7Bits)
 import Sound.Midi.Internal.Types (PitchWheel (..))
 
-highest   = splitBytes 16383
-higher    = splitBytes 13652
-high      = splitBytes 10921
-centered  = splitBytes 8192
-low       = splitBytes 5461
-lower     = splitBytes 2730
-lowest    = splitBytes 1
+highest   = pitchWheel 100
+higher    = pitchWheel 66.6
+high      = pitchWheel 33.
+centered  = pitchWheel 0
+low       = pitchWheel (-33.3)
+lower     = pitchWheel (-66.6)
+lowest    = pitchWheel (-100)
 
 pitchWheel :: Float -> PitchWheel
 pitchWheel 0 = centered
-pitchWheel n = splitBytes $ correlateRanges (-100,100) (0,16383) n
+pitchWheel n = PitchWheel . splitBytes $ correlateRanges (-100,100) (0,16383) n
 
 -- the PitchWheel value is is a 14-bit uint represented by two 8-bit uints,
 -- where the first byte's bits 0-6 are the value's bits 0-6 and the second
 -- byte's bits 0-6 are the value's bits 7-13
-splitBytes :: Int -> PitchWheel
-splitBytes n = PitchWheel $ fromIntegral bits
-  where
-    bits = shift (mask7Bits n) 8 .|. mask7Bits (shiftR n 7)
+splitBytes :: Int -> Word16
+splitBytes n = fromIntegral $ shiftL (mask7Bits n) 8 .|. mask7Bits (shiftR n 7)
