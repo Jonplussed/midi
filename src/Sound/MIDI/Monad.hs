c93f2949dc1161468c1f6ea28cb01110db20274c
Jon Childress <jon@childr.es>
2015-09-07 10:12:11 -0500

    first pass at MIDI free monad; still needs to encode size

--- src/Sound/MIDI/Monad.hs
+++ src/Sound/MIDI/Monad.hs
@@ -1,9 +1,15 @@
-module Sound.Midi.Monad where
+module Sound.Midi.Monad
+( Track
+, TrackF (..)
+, runTrack
+) where
 
-import Control.Monad.Free
-import Data.Binary.Put
+import Control.Monad.Free (Free (..))
+import Data.Binary.Put (Put, putWord8, putWord16be)
+
+import qualified Sound.Midi.Values.VoiceEvent as VE
+
 import Sound.Midi.Internal.Types
-import Sound.Midi.Values.VoiceEvent
 
 data TrackF next
   = NoteOff DeltaTime Note Velocity next
@@ -12,7 +18,7 @@ data TrackF next
   | ControlChange DeltaTime ControllerIdent ControllerValue next
   | PatchChange DeltaTime Patch next
   | ChannelPressure DeltaTime Pressure next
-  | PitchWheelChange DeltaTime next
+  | PitchWheelChange DeltaTime PitchWheel next
 
 type Track a = Free TrackF a
 
@@ -21,25 +27,48 @@ runTrack chan = interp
   where
     interp (Free (NoteOff dt (Note note) (Velocity vel) next)) = do
       putDeltaTime dt
-      putEvent noteOff chan
+      putEvent VE.noteOff chan
       putWord8 note
       putWord8 vel
       interp next
     interp (Free (NoteOn dt (Note note) (Velocity vel) next)) = do
       putDeltaTime dt
-      putEvent noteOn chan
+      putEvent VE.noteOn chan
       putWord8 note
       putWord8 vel
       interp next
     interp (Free (AfterTouch dt (Note note) (Pressure pres) next)) = do
       putDeltaTime dt
-      putEvent afterTouch chan
+      putEvent VE.afterTouch chan
       putWord8 note
       putWord8 pres
       interp next
+    interp (Free (ControlChange dt (ControllerIdent ident) (ControllerValue val) next)) = do
+      putDeltaTime dt
+      putEvent VE.controlChange chan
+      putWord8 ident
+      putWord8 val
+      interp next
+    interp (Free (PatchChange dt (Patch patch) next)) = do
+      putDeltaTime dt
+      putEvent VE.patchChange chan
+      putWord8 patch
+      interp next
+    interp (Free (ChannelPressure dt (Pressure pres) next)) = do
+      putDeltaTime dt
+      putEvent VE.channelPressure chan
+      putWord8 pres
+      interp next
+    interp (Free (PitchWheelChange dt (PitchWheel pitch) next)) = do
+      putDeltaTime dt
+      putEvent VE.pitchWheelChange chan
+      putWord16be pitch
+      interp next
+
+-- private functions
 
 putDeltaTime :: DeltaTime -> Put
 putDeltaTime (DeltaTime words) = mapM_ putWord8 words
 
 putEvent :: VoiceEvent -> Channel -> Put
-putEvent (VoiceEvent ve) = putWord8 . ve
+putEvent (VoiceEvent withChan) = putWord8 . withChan
